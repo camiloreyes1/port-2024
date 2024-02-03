31229ae4285371be6645a1026a3f8a69bc8c874d
Juan Reyes <camiloreyes1125@gmail.com>
2024-02-03 18:04:10 -0500

    about me page

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,13 +1,12 @@
-
 const AboutMe = () => {
   return (
-    <div className="about-me-container">
+    <div style={{ fontFamily: 'Protest Strike, sans-serif', padding: '12%'}}>
       <h1>Welcome</h1>
-      <p>
+      <h2>
         Hi! My name is Juan Reyes. As a Full Stack Developer, I have had the privilege of working on a diverse range of projects, from creating interactive web applications to building robust backend systems. My dedication to mastering both frontend and backend technologies enables me to bring comprehensive solutions to life.
         <br /><br />
         In this portfolio, you will find examples of my coding problem-solving abilities and a commitment to clean, efficient, and scalable code. Whether you are an employer seeking a capable developer or a fellow enthusiast of the tech world, I invite you to explore my projects and get to know my capabilities.
-      </p>
+      </h2>
     </div>
   );
 }
