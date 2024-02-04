2878cc08b2a20ca59807d9f5e09a038ad98a5c55
Juan Reyes <camiloreyes1125@gmail.com>
2024-02-04 14:33:45 -0500

    projects page commit

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,6 +1,6 @@
 const AboutMe = () => {
   return (
-    <div style={{ fontFamily: 'Protest Strike, sans-serif', padding: '12%'}}>
+    <div style={{ fontFamily: 'Protest Strike, sans-serif', padding: '12%', paddingBottom: '0%'}}>
       <h1>Welcome</h1>
       <h2>
         Hi! My name is Juan Reyes. As a Full Stack Developer, I have had the privilege of working on a diverse range of projects, from creating interactive web applications to building robust backend systems. My dedication to mastering both frontend and backend technologies enables me to bring comprehensive solutions to life.
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,11 +7,14 @@ import Typography from '@mui/material/Typography';
 
 export default function Projects() {
   return (
-    <Card sx={{ maxWidth: 445 }}>
+    <div style={{ fontFamily: 'Protest Strike, sans-serif', paddingLeft: '12%', paddingRight: '12%'}}>
+    <h1>My Projects</h1>
+
+    <Card sx={{ maxWidth: 445, marginBottom: '5%' }}>
       <CardMedia
         component="img"
         alt="green iguana"
-        height="140"
+        height="260"
         image="https://res.cloudinary.com/delscyuhi/image/upload/v1695872659/tvptex45kwu0sluvn7nq.png"
       />
       <CardContent>
@@ -28,6 +31,73 @@ export default function Projects() {
         <Button size="small">Use App</Button>
       </CardActions>
     </Card>
+
+    <Card sx={{ maxWidth: 445, marginBottom: '5%' }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="220"
+        image="https://res.cloudinary.com/delscyuhi/image/upload/v1695871890/qb2hjytc1vxvgxerstal.png"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          MRKT
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Demo Video</Button>
+        <Button size="small">Use App</Button>
+      </CardActions>
+    </Card>
+
+    <Card sx={{ maxWidth: 445, marginBottom: '5%' }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="270"
+        image="https://res.cloudinary.com/delscyuhi/image/upload/v1695927673/d89mxo4bugoo0sx2lkv6.png"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Pixels
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Demo Video</Button>
+        <Button size="small">Use App</Button>
+      </CardActions>
+    </Card>
+
+    <Card sx={{ maxWidth: 445, marginBottom: '5%' }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="270"
+        image="https://res.cloudinary.com/delscyuhi/image/upload/v1695871890/t65digbrgsxxjcqllxen.png"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Chicken Run
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Demo Video</Button>
+        <Button size="small">Use App</Button>
+      </CardActions>
+    </Card>
+    </div>
 
   );
 }
